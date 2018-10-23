d05ab248c7aedad14671082087a9e535ef3ec7b9
John Weiss <jmweiss87@gmail.com>
2018-10-23 16:15:42 -0400

    Added JS function for links using .html method instead of using seperate html pages

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 // jQuery.getJSON  url [, data ] [, success ] 
 state = {};
 
+const pages = [
+  {
+    title: "Home",
+    body: "Home Page"
+  },
+  {
+    title: "Solutions",
+    body: "Solutions Page"
+  },
+  {
+    title: "About",
+    body: "About Page"
+  },
+  {
+    title: "Cotact",
+    body: "Contact Page"
+  }
+]
+
 /* Makes the AJAX request */
 function getPwned(account, successHandler){
   console.log("account: ", account);
@@ -55,3 +74,51 @@ function renderBreaches(breach, index){
 $(watchSubmit);
 
 //<img src=${breach.LogoType}>
+
+
+/* HTML Links */
+
+$('.js-link').on('click', function (event){
+  event.preventDefault();
+  // let index= event.val();
+  // the above did not work, this.value and event.currentTarget.value both worked same.
+  // would like to swtich back to val() instead of this.value...looking into.
+  // the lines 87 and 88 are the same fyi.
+  console.log(this.value);
+  // console.log(event.currentTarget.value);
+  let index = this.value;
+  $('#js-results').html(  
+
+  ` 
+    <h1>Test</h1>
+    <h2>${pages[index].title}</h2>
+    <h3>${pages[index].body}</h3>
+    <br>
+  `      
+  )
+});
+
+// getting uncaught typererror, cannot read property of title of undefined
+// Now the number values are connected, 0,1,2,3...
+// need to have them pull up the content now. ******
+
+//Ah! so how are we conntecting it to our object??
+
+// example code: 
+// $('main').html(pages[1].body);
+// So, what I need is to replace main with #js-results
+// that is done so now, the 1 inside pages (the object) needs to be a variable value
+// and this variable value needs to select the content from my object pages...
+// like his example pages[1].body
+// how am I going to get it to not only select each of the 4 indeces of the object
+// but then I need to select the keys inside of it! 
+// the quizApp may have some answers / refresh my memory.
+
+// and if statement might do the trick. so might a loop. HERE****
+
+// let index = this.val();
+// inside event listener, will get whatever is inside html attribute
+// values of 0-3
+// will reference const pages array...indexes to ref const pages obj w/ content.
+
+// value can be used in both button and list , the a anchor tag cannot have value
