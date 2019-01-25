7e3b2eb6fe9ce0cd3c1317773da242098e88c043
John Weiss <jmweiss87@gmail.com>
2019-01-25 10:28:20 -0500

    Added NewsOrg API call and now call to haveIBeenPwned API is resulting in a 403 error. Posting this for debug.

--- js/index.js
+++ js/index.js
@@ -9,12 +9,12 @@ const pages = [
   },
   {
     title: "Solutions",
-    body: "Suggestions for Computer Security and Best Practices."
+    body: "Suggestions for Computer Security and Best Practices. Will type up content when the call to the api is working again. Frustrating."
   },
   {
     title: "About Me",
     subtitle: "John Weiss",
-    body: "Hey there! Thanks for visiting my page!" + " This is a project I put together to practice making API calls and displaying the data." 
+    body: "Hey there! Thanks for visiting my page." + " This is a project I put together to practice making AJAX calls to APIs, haveIbeenPwned and NewsOrg, and displaying the data." 
      + " I went to University of South Carolina for college, have done various front-end freelance projects, and am working" 
      + " on becoming a full stack developer. I hope you enjoy this application, and find use in the information provided to better protect your data.",
     img: "addOne.png"
@@ -30,6 +30,12 @@ const pages = [
 function getPwned(account, successHandler){
   console.log("account: ", account);
   $.getJSON( `https://haveibeenpwned.com/api/v2/breachedaccount/${account}`, successHandler);
+}
+
+/* Second AJAX request */
+function newsRequest(data){
+  console.log(data);
+  $.getJSON(`https://newsapi.org/v2/everything?q=${data}%20AND%20(hack%20OR%20OR%20breach)%20data&apiKey=f62ec347bf434ebba702168d677d54a0`, newsSuccessHandler);
 }
 
 /* Handles the event when the user submits the email address */
@@ -50,8 +56,33 @@ function submitHandler(event) {
 function successHandler(data) {
   console.log("DATA", data);
   showSearchResults();
+  
+  // data argument is live, has all the info we need to make an api call for each company
+  // could grab the title here, and then *map* the company names, send to newsRequest
+  // newsData = newsSuccessHandler(breaches[0]);
+  // newsRequest(breaches[1]);
+  // so for each of the responses, i need to reference the title, to start. (eventually two)
+  // need to iterate through each...
+  // send it to the newsSHandler, then do the mapping there. i think.
+  // set a variable here? then send it to newsRequest
+  // newsData = breaches.map((company, index));
+  // newsRequest(newsData);
+  newsData = data.map(company => newsRequest(company.Title));
+  // $('#js-results').html(newsData);
+  // an FYI on .map method, if using arrow function, only close parenthesis on left side if TWO arguments.
   const breaches = data.map((breach, index) => renderBreaches(breach, index));
   $('#js-results').html(breaches);
+  // if you could add the newsData to breaches on line 72 before you put breaches into the html, that would be ideal.
+}
+
+// newsSuccessHandler(newsData); this should be sent to the dom....
+
+// SuccessHandler for News API
+// deal with results from news api, remember it goes for each api request, *for each** company a request
+function newsSuccessHandler(result){
+  console.log(result);
+  // take the info from results object and send it to the dom...
+  // how to display on the DOM.....use .html or another method
 }
 
 
@@ -61,6 +92,7 @@ function renderBreaches(breach, index){
  <h2>Domain: ${breach.Domain}</h2>
  <h3>Breach Description: ${breach.Description}</h3>
  </div>`;}
+ // shouldn't this semi-colon be outside the bracket?** Just making sure..
 
 
 $(watchSubmit);
